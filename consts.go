a085602c4fbc3a5f1f39c4ffec204e0447556d5b
Srinidhi Kaushik <shrinidhi.kaushik@gmail.com>
2021-01-31 15:26:18 +0530

    Move split-read.

--- consts.go
+++ consts.go
@@ -15,19 +15,28 @@ const (
 
 	// pidSmaps and pidMem are paths to the process's
 	// memory mapping and memory on `procfs' respectively.
-	pidSmaps = "/proc/%d/smaps"
+	pidSmaps = "./proc/%d/smaps"
 	pidMem   = "/proc/%d/mem"
 
-	// pageSz is the amount of memory to be read from "/proc/{pid}/mem"
-	// per read operation (assuming 4 kB pages).
-	pageSz = int64(4096)
-
 	// For logging.
-	yankLogPfx  = "de-swap:  * %s: region[0x%012x-0x%012x]: %8d kB"
-	yankErrMsg  = "ERR  (%s: %v)\n"
+	mainRunMsg = "de-swap: main:\tPID %d (theads: %d, split: %s, " +
+		"no-op: %v)\n"
+	mainEndMsg = "de-swap: main:\tOK; moved %d kB in %s\n"
+	mainErrMsg = "de-swap: main:\tERR: %s\n"
+	argsErrMsg = "de-swap: args:\tERR: %s\n"
+
+	smapsSwapMsg = "de-swap: smaps:\t%s: in-swap: %d kB, regions: %d\n"
+	smapsReadMsg = "de-swap: smaps:\t%s: to-read: %d kB, regions: %d\n"
+	splitLogMsg  = "de-swap: split:\tinit: %d, brk: %d, big: %d, " +
+		"skip: %d, overflow: %s\n"
+
+	yankLogPfx  = "de-swap: yank:\t%s: region[0x%012x-0x%012x]: %8d kB"
 	yankOKMsg   = "OK\n"
-	yankWarnMsg = "WARN (read-fail: %d/%d)\n"
+	yankWarnMsg = "WARN:\tread-fail: %-8d/%8d)\n"
+	yankErrMsg  = "ERR:\t%s: %v)\n"
 	yankNoopMsg = "NO-OP\n"
 	yankDbgMsg  = "%s, read(from=0x%012x, to=0x%012x): " +
 		"got/exp: %8d/%-8d B (err: %v)\n"
+
+	swapErrMsg = "swap: failed to swap-in some memory (read-diff: %d kB)"
 )
