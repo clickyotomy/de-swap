84dfeef84c3ba6f480e6e3a9990ad77ac7d4ae9e
Srinidhi Kaushik <shrinidhi.kaushik@gmail.com>
2021-01-31 15:30:27 +0530

    Fix logging, parse new option.

--- main.go
+++ main.go
@@ -19,34 +19,51 @@ var (
 	dryRun = false
 	debug  = 0
 
-	// Total number of kilobytes swapped out (approximately).
-	swappedOut = uint64(0)
+	// pageSz is the amount of memory to be read (in chunks)
+	// from "/proc/{pid}/mem" per read operation. By default,
+	// it is set to 4 kB.
+	pageSz = uint64(4 * 1024)
+
+	// overflow is the threshold for swapped-out memory in a
+	// region that triggers a split operation in that region (break
+	// it into smaller chunks) for better performance. By default,
+	// it is set to 64 kB.
+	overflow = uint64(64 * 1024)
+
+	// Total number of kilobytes read and swapped-in,
+	mRead = uint64(0)
+	mSwap = uint64(0)
 )
 
 // mmappedSwap holds the range of the "mmap()"-ed region
 // which contains the swapped memory and its size.
 type mmapSwapRegion struct {
-	sz  uint64 // Size.
-	off int64  // Begin offset (for seeking).
-	end int64  // End of region.
+	sz    uint64 // Number of kB in swap-space.
+	off   uint64 // Begin offset (for seeking).
+	end   uint64 // End of region.
+	split bool   // Has been split into smaller regions.
 }
 
 // deSwap brings back a process's memory from the swap-space.
-func deSwap(pid, routines int) error {
+func deswap(pid, routines uint) error {
 	var (
 		err error
 		wg  sync.WaitGroup
 		ent mmapSwapRegion
+		nr  int
+		thr uint
 
 		swp = make([]mmapSwapRegion, 0)
 		mch = make(chan mmapSwapRegion)
 		mem = fmt.Sprintf(pidMem, pid)
-		tot = uint64(0)
+		smp = fmt.Sprintf(pidSmaps, pid)
+		rd  = uint64(0)
+		sw  = uint64(0)
 	)
 
 	// Read all the entries at once because we don't
 	// want to keep the file open for a long time.
-	if err = loadSmaps(fmt.Sprintf(pidSmaps, pid), &swp); err != nil {
+	if err = smaps(smp, &swp); err != nil {
 		return err
 	}
 
@@ -54,12 +71,33 @@ func deSwap(pid, routines int) error {
 		return nil
 	}
 
+	// Get the stats for swapped-in memory.
+	for _, ent = range swp {
+		if !ent.split {
+			sw += ent.sz
+			nr++
+		}
+	}
+	fmt.Fprintf(os.Stdout, fmt.Sprintf(smapsSwapMsg, smp, (sw/1024), nr))
+
+	// Split the regions into smaller chunks; loop again
+	// for total readable memory (after the splitting).
+	split(&swp)
+	nr = 0
+	for _, ent = range swp {
+		if !ent.split {
+			rd += ent.sz
+			nr++
+		}
+	}
+	fmt.Fprintf(os.Stdout, fmt.Sprintf(smapsReadMsg, smp, (rd/1024), nr))
+
 	// Start sending data to the channel.
 	wg.Add(1)
 	go stream(mch, &swp, &wg)
 
 	// Spawn workers to "yank" memory out of the swap-space.
-	for i := 0; i < routines; i++ {
+	for thr = 0; thr < routines; thr++ {
 		wg.Add(1)
 		go yank(mch, mem, &wg)
 	}
@@ -67,18 +105,15 @@ func deSwap(pid, routines int) error {
 	// Wait for all go-routines to finish.
 	wg.Wait()
 
-	for _, ent = range swp {
-		tot += ent.sz
+	if atomic.LoadUint64(&mRead) != rd && !dryRun {
+		return fmt.Errorf(fmt.Sprintf(
+			swapErrMsg,
+			((rd - atomic.LoadUint64(&mRead)) / 1024),
+		))
+
 	}
 
-	if atomic.LoadUint64(&swappedOut) != tot && !dryRun {
-		return fmt.Errorf(
-			"swap: failed to swap-in some "+
-				"memory regions; diff: %d kB",
-			(tot - atomic.LoadUint64(&swappedOut)),
-		)
-	}
-
+	mSwap = sw
 	return nil
 }
 
@@ -86,11 +121,12 @@ func main() {
 	flag.Usage = help
 
 	var (
-		pid = flag.Int("p", -1, "PID")
-		thr = flag.Int("j", 1, "number of threads")
+		pid = flag.Uint("p", 0, "PID")
 		dry = flag.Bool("n", false, "no-op")
+		thr = flag.Uint("j", 1, "number of threads")
+		ssz = flag.Uint64("r", overflow, "region split size (bytes)")
 		dbg = flag.Bool("v", false, "debug")
-		vrb = flag.Bool("vv", false, "debug (verbose)")
+		vrb = flag.Bool("vv", false, "extra debug")
 
 		// For tracking the time elapsed.
 		now = time.Now()
@@ -100,25 +136,32 @@ func main() {
 	flag.Parse()
 
 	if *pid <= 0 {
-		fmt.Fprintf(
-			os.Stderr,
-			"de-swap: ERR: invalid or no PID specified\n",
-		)
+		fmt.Fprintf(os.Stderr, fmt.Sprintf(
+			argsErrMsg, "invalid or no PID specified",
+		))
 		os.Exit(2)
 	}
 
 	// Doesn't matter, just use 1.
 	if *thr <= 0 {
-		fmt.Fprintf(
-			os.Stderr,
-			"de-swap: ERR: bad thread value; using defaults\n",
-		)
+		fmt.Fprintf(os.Stderr, fmt.Sprintf(
+			argsErrMsg, "invalid thread value",
+		))
+		os.Exit(2)
+	}
 
-		*thr = 1
+	if (*ssz <= 0) || (*ssz%2 != 0) || ((*ssz & (*ssz - 1)) != 0) {
+		fmt.Fprintf(os.Stderr, fmt.Sprintf(
+			argsErrMsg, "bad read split size value",
+		))
+		os.Exit(2)
 	}
 
 	// For no-op mode.
 	dryRun = *dry
+
+	// Set read split size value.
+	overflow = *ssz
 
 	// For debug and verbose output.
 	if *dbg {
@@ -129,19 +172,17 @@ func main() {
 		debug = 2
 	}
 
-	fmt.Fprintf(os.Stdout,
-		"de-swap: running for PID %d (no-op: %v, threads: %d)\n",
-		*pid, dryRun, *thr,
-	)
+	fmt.Fprintf(os.Stdout, fmt.Sprintf(
+		mainRunMsg, *pid, *thr, fmtBytes(overflow), dryRun,
+	))
 
-	if err := deSwap(*pid, *thr); err != nil {
-		fmt.Fprintf(os.Stderr, "de-swap: ERR: %s\n", err)
+	if err := deswap(*pid, *thr); err != nil {
+		fmt.Fprintf(os.Stderr, fmt.Sprintf(mainErrMsg, err))
 		os.Exit(1)
 	} else {
 
-		fmt.Fprintf(os.Stdout,
-			"de-swap: OK; moved %d kB in %s\n",
-			atomic.LoadUint64(&swappedOut), time.Since(now),
-		)
+		fmt.Fprintf(os.Stdout, fmt.Sprintf(
+			mainEndMsg, (mSwap/1024), time.Since(now),
+		))
 	}
 }
